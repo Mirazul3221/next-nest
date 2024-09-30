57462030c9b9b7ee1420f0e224ecbc0fe3627bfb
Mirazul3221 <mirazul3221@gmail.com>
2024-09-30 15:11:58 +0600

    U

--- client/app/userdashboard/searchusers/[user]/page.js
+++ client/app/userdashboard/searchusers/[user]/page.js
@@ -16,6 +16,7 @@ import { toast, ToastContainer } from "react-toastify";
 import SuperHeader from "../../components/SuperHeader";
 import Link from "next/link";
 import Messanger from "../../components/messanger/Messanger";
+import { useSocket } from "../../global/SocketProvider";
 
 const Page = () => {
   const pram = useParams();
@@ -197,6 +198,7 @@ const Page = () => {
   //    setSomeFriendProfileAndId(data)
   //   }
   console.log(acceptedFriendsId);
+  const {myActiveFriends} = useSocket()
   return (
     <ProtectRoute>
       {/* <div onClick={someFriendProfileAndIdd}>jsgdkgjsdkjgjkzdjis</div> */}
@@ -462,17 +464,17 @@ const Page = () => {
                       key={i}
                       className="mt-4 bg-gray-100 p-4 rounded-lg border"
                     >
-                      <div className="relative rounded-full bg-black w-fit mx-auto">
-                        {item?.isOnline ? (
-                          <div className="w-5 h-5 border-4 border-white bg-green-500 absolute rounded-full right-10 bottom-1"></div>
-                        ) : (
-                          <div className="w-5 h-5 border-4 border-white bg-[#e7523a] absolute rounded-full right-10 bottom-1"></div>
-                        )}
-                        <img
-                          className="w-48 mx-auto border-2 md:border-4 rounded-full"
-                          src={item.profile}
-                        />
-                      </div>
+                    <div className="relative rounded-full bg-black w-fit mx-auto">
+                      { myActiveFriends && myActiveFriends?.some(O=> O === item._id) ? (
+                        <div className="w-5 h-5 border-2 border-white bg-green-500 absolute rounded-full right-10 bottom-1"></div>
+                      ) : (
+                        <div className="w-5 h-5 border-2 border-white bg-gray-400 absolute rounded-full right-10 bottom-1"></div>
+                      )}
+                      <img
+                        className="w-48 mx-auto border-2 md:border-4 rounded-full"
+                        src={item.profile}
+                      />
+                    </div>
                       <h2 className="text-center text-2xl font-semibold text-gray-700 mt-2">
                         {item.name}
                       </h2>
