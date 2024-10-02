5ee90396fa6718b8bfc71d78477d33c7b427451b
Mirazul3221 <mirazul3221@gmail.com>
2024-10-03 01:41:11 +0600

    U

---
+++ client/app/userdashboard/audio-video-call-center/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+import { useSearchParams } from 'next/navigation'
+import React from 'react'
+
+const Page = () => {
+const test = useSearchParams()
+  return (
+    <div>
+        page
+    </div>
+  )
+}
+
+export default Page
