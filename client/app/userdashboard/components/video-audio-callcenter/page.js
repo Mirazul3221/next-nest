aa9bb8a271b5cf3da59caaebe4bb9e7831b8f5bc
Mirazul3221 <mirazul3221@gmail.com>
2024-10-03 00:33:35 +0600

    U

--- client/app/userdashboard/components/video-audio-callcenter/page.js
+++ client/app/userdashboard/components/video-audio-callcenter/page.js
@@ -1,10 +1,7 @@
 'use client'
-import { useSearchParams } from 'next/navigation';
 import React from 'react'
 
 const Page = () => {
-    const data = useSearchParams();
-    console.log(data)
   return (
     <div>
         hello
